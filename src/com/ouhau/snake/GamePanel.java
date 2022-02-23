c53704be1d0e04fb19459b5a6f54bd7318c5325f
Admin <75502881@qq.com>
2022-02-23 21:17:39 +0900

    sdf

--- src/com/ouhau/snake/GamePanel.java
+++ src/com/ouhau/snake/GamePanel.java
@@ -197,6 +197,7 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
 
 
                 //test
+                //testds
                 foodX = 25 + 25 * random.nextInt(34);
                 foodY = 75 + 25 * random.nextInt(24);
             }
