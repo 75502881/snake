b078ec59c380dfbbb75f67e7b3d0c9e9addc9314
Admin <75502881@qq.com>
2022-02-23 21:17:17 +0900

    ouhaocheng

--- src/com/ouhau/snake/GamePanel.java
+++ src/com/ouhau/snake/GamePanel.java
@@ -193,7 +193,7 @@ public class GamePanel extends JPanel implements KeyListener, ActionListener {
                     imageIcon = Data.strawberry;
                 }
 
-                //test
+                //testssfsd
                 foodX = 25 + 25 * random.nextInt(34);
                 foodY = 75 + 25 * random.nextInt(24);
             }
