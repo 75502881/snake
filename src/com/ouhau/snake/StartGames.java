5df387fbf663faf50c5a187cce1d129802cfebe9
Admin <75502881@qq.com>
2022-02-23 19:30:16 +0900

    snake Game

---
+++ src/com/ouhau/snake/StartGames.java
@@ -0,0 +1,16 @@
+package com.ouhau.snake;
+
+import javax.swing.*;
+
+public class StartGames {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("欧がゲームで遊ぶ");
+        frame.setBounds(10,10,900,720);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(new GamePanel());
+
+        frame.setVisible(true);
+    }
+}
