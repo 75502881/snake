a80d5fe4bdfcc03d332be64912d2252eebac1209
Admin <75502881@qq.com>
2022-02-24 21:34:49 +0900

    sdf

---
+++ src/com/ouhau/snake/bt/Data.java
@@ -0,0 +1,32 @@
+package com.ouhau.snake.bt;
+
+import javax.swing.*;
+import java.net.URL;
+
+public class Data {
+    public static URL headerURL = Data.class.getResource("/statics/header.png");
+    public static URL upUrl = Data.class.getResource("/statics/up.png");
+    public static URL downURL = Data.class.getResource("/statics/down.png");
+    public static URL leftURL = Data.class.getResource("/statics/left.png");
+    public static URL rightURL = Data.class.getResource("/statics/right.png");
+    public static URL bodyURL = Data.class.getResource("/statics/body.png");
+    public static URL foodURL = Data.class.getResource("/statics/food/food.png");
+    public static URL watermelonURL = Data.class.getResource("/statics/food/Watermelon.png");
+    public static URL bombURL = Data.class.getResource("/statics/food/bomb.png");
+    public static URL appleURL = Data.class.getResource("/statics/food/apple.png");
+    public static URL deathURL = Data.class.getResource("/statics/food/death.png");
+    public static URL strawberryURL = Data.class.getResource("/statics/food/strawberry.jpg");
+    public static ImageIcon header = new ImageIcon(headerURL);
+    public static ImageIcon up = new ImageIcon(upUrl);
+    public static ImageIcon down = new ImageIcon(downURL);
+    public static ImageIcon left = new ImageIcon(leftURL);
+    public static ImageIcon right = new ImageIcon(rightURL);
+    public static ImageIcon body = new ImageIcon(bodyURL);
+    public static ImageIcon food = new ImageIcon(foodURL);
+    public static ImageIcon watermelon = new ImageIcon(watermelonURL);
+    public static ImageIcon bomb = new ImageIcon(bombURL);
+    public static ImageIcon apple = new ImageIcon(appleURL);
+    public static ImageIcon death = new ImageIcon(deathURL);
+    public static ImageIcon strawberry = new ImageIcon(strawberryURL);
+
+}
---
+++ src/com/ouhau/snake/bt/GamePanel.java
@@ -0,0 +1,200 @@
+package com.ouhau.snake.bt;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements KeyListener, ActionListener {
+
+    int lenth;
+    int score;
+    int[] snakeX = new int[600];
+    int[] snakeY = new int[600];
+    String fx;
+
+    boolean isStart = false;
+    Timer timer = new Timer(100, this);
+
+    int foodX;
+    int foodY;
+    Random random = new Random();
+    Random num = new Random();
+    Random num2 = new Random();
+    ImageIcon imageIcon = Data.food;
+
+    public GamePanel() {
+        init();
+        this.setFocusable(true);
+        this.addKeyListener(this);
+        timer.start();
+    }
+
+    boolean isFail = false;
+
+    public void init() {
+        lenth = 3;
+        snakeX[0] = 100;
+        snakeY[0] = 100;
+        snakeX[1] = 75;
+        snakeY[1] = 100;
+        snakeX[2] = 50;
+        snakeY[2] = 100;
+        fx = "R";
+
+        foodX = 25 + 25 * random.nextInt(34);
+        foodY = 75 + 25 * random.nextInt(24);
+
+        score = 0;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.WHITE);
+
+        Data.header.paintIcon(this, g, 25, 11);
+
+        g.fillRect(25, 75, 850, 600);
+
+        if (fx.equals("R")) {
+            Data.right.paintIcon(this, g, snakeX[0], snakeY[0]);
+        } else if (fx.equals("L")) {
+            Data.left.paintIcon(this, g, snakeX[0], snakeY[0]);
+        } else if (fx.equals("U")) {
+            Data.up.paintIcon(this, g, snakeX[0], snakeY[0]);
+        } else if (fx.equals("D")) {
+            Data.down.paintIcon(this, g, snakeX[0], snakeY[0]);
+        }
+
+        for (int i = 1; i < lenth; i++) {
+            Data.body.paintIcon(this, g, snakeX[i], snakeY[i]);
+        }
+        imageIcon.paintIcon(this, g, foodX, foodY);
+
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("状態",Font.BOLD,18));
+        g.drawString("長さ"+lenth,750,35);
+        g.drawString("スコア"+score,750,50);
+
+        if (isStart == false) {
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("sanke Game", Font.BOLD, 40));
+            g.drawString("スペースでスタート", 300, 300);
+        }
+
+        if (isFail) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("sanke Game", Font.BOLD, 40));
+            g.drawString("死んでしまった", 300, 300);
+        }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int kyeCode = e.getKeyCode();
+
+        if (kyeCode == KeyEvent.VK_SPACE) {
+            if (isFail) {
+                isFail = false;
+                init();
+            } else {
+                isStart = !isStart;
+            }
+            repaint();
+        }
+
+        if (kyeCode == KeyEvent.VK_DOWN) {
+            fx = "D";
+        } else if (kyeCode == KeyEvent.VK_UP) {
+            fx = "U";
+        } else if (kyeCode == KeyEvent.VK_LEFT) {
+            fx = "L";
+        } else if (kyeCode == KeyEvent.VK_RIGHT) {
+            fx = "R";
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (isStart && isFail == false) {
+            for (int i = lenth - 1; i > 0; i--) {
+                snakeX[i] = snakeX[i - 1];
+                snakeY[i] = snakeY[i - 1];
+            }
+
+            if (fx.equals("R")) {
+                snakeX[0] = snakeX[0] + 25;
+                if (snakeX[0] > 850) {
+                    snakeX[0] = 25;
+                }
+            } else if (fx.equals("L")) {
+                snakeX[0] = snakeX[0] - 25;
+                if (snakeX[0] < 25) {
+                    snakeX[0] = 850;
+                }
+            } else if (fx.equals("U")) {
+                snakeY[0] = snakeY[0] - 25;
+                if (snakeY[0] < 75) {
+                    snakeY[0] = 650;
+                }
+            } else if (fx.equals("D")) {
+                snakeY[0] = snakeY[0] + 25;
+                if (snakeY[0] > 650) {
+                    snakeY[0] = 75;
+                }
+            }
+
+            if (snakeX[0] == foodX && snakeY[0] == foodY) {
+                lenth++;
+
+                score+=10;
+
+                int nums = num.nextInt(4);
+                int nums2 = num2.nextInt(2);
+                if (nums == 1) {
+                    imageIcon = Data.food;
+                } else if (nums == 2) {
+                    if (nums2 == 1){
+                        imageIcon = Data.watermelon;
+                    }else if (nums2 == 2){
+                        imageIcon = Data.strawberry;
+                    }
+                } else if (nums == 3) {
+                     imageIcon = Data.bomb;
+                } else if (nums == 4) {
+                    if (nums2 == 1){
+                        imageIcon = Data.apple;
+                    }else if (nums2 == 2){
+                        imageIcon = Data.death;
+                    }
+                }
+
+                foodX = 25 + 25 * random.nextInt(34);
+                foodY = 75 + 25 * random.nextInt(24);
+            }
+
+            for (int i = 1; i < lenth; i++){
+                if (snakeX[0] == snakeX[i] && snakeY[0] == snakeY[i]){
+                    isFail = true;
+                }
+            }
+
+            repaint();
+        }
+        timer.start();
+    }
+}
+
