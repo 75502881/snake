63935e3e2f0b470864924bff4dc8f42410e6b6cc
Admin <75502881@qq.com>
2022-02-24 21:28:21 +0900

    sdf

--- src/com/ouhau/snake/Data2.java
+++ src/com/ouhau/snake/Data2.java
@@ -3,7 +3,7 @@ package com.ouhau.snake;
 import javax.swing.*;
 import java.net.URL;
 
-public class Data {
+public class Data2 {
 //    public static URL headerURL = Data.class.getResource("/statics/header.png");
 //    public static URL upUrl = Data.class.getResource("/statics/up.png");
 //    public static URL downURL = Data.class.getResource("/statics/down.png");
@@ -28,18 +28,18 @@ public class Data {
 //    public static ImageIcon apple = new ImageIcon(appleURL);
 //    public static ImageIcon death = new ImageIcon(deathURL);
 //    public static ImageIcon strawberry = new ImageIcon(strawberryURL);
-    public static URL headerURL = Data.class.getResource("/statics/header.png");
-    public static URL upUrl = Data.class.getResource("/statics/up.png");
-    public static URL downURL = Data.class.getResource("/statics/down.png");
-    public static URL leftURL = Data.class.getResource("/statics/left.png");
-    public static URL rightURL = Data.class.getResource("/statics/right.png");
-    public static URL bodyURL = Data.class.getResource("/statics/body.png");
-    public static URL foodURL = Data.class.getResource("/statics/food/food.png");
-    public static URL watermelonURL = Data.class.getResource("/statics/food/Watermelon.png");
-    public static URL bombURL = Data.class.getResource("/statics/food/bomb.png");
-    public static URL appleURL = Data.class.getResource("/statics/food/apple.png");
-    public static URL deathURL = Data.class.getResource("/statics/food/death.png");
-    public static URL strawberryURL = Data.class.getResource("/statics/food/strawberry.jpg");
+    public static URL headerURL = Data2.class.getResource("/statics/header.png");
+    public static URL upUrl = Data2.class.getResource("/statics/up.png");
+    public static URL downURL = Data2.class.getResource("/statics/down.png");
+    public static URL leftURL = Data2.class.getResource("/statics/left.png");
+    public static URL rightURL = Data2.class.getResource("/statics/right.png");
+    public static URL bodyURL = Data2.class.getResource("/statics/body.png");
+    public static URL foodURL = Data2.class.getResource("/statics/food/food.png");
+    public static URL watermelonURL = Data2.class.getResource("/statics/food/Watermelon.png");
+    public static URL bombURL = Data2.class.getResource("/statics/food/bomb.png");
+    public static URL appleURL = Data2.class.getResource("/statics/food/apple.png");
+    public static URL deathURL = Data2.class.getResource("/statics/food/death.png");
+    public static URL strawberryURL = Data2.class.getResource("/statics/food/strawberry.jpg");
     public static ImageIcon header = new ImageIcon(headerURL);
     public static ImageIcon up = new ImageIcon(upUrl);
     public static ImageIcon down = new ImageIcon(downURL);
