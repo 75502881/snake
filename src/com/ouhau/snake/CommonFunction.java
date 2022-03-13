9cdeebe31209b0fe4ff245abc4c83626f8b42089
Admin <75502881@qq.com>
2022-03-13 20:48:34 +0900

    snake Game

---
+++ src/com/ouhau/snake/CommonFunction.java
@@ -0,0 +1,9 @@
+package com.ouhau.snake;
+
+import com.ouhau.snake.GamePanel.*;
+
+public class function {
+    public static void isVisible(boolean bool){
+
+    }
+}
---
+++ src/com/ouhau/snake/Function.java
@@ -0,0 +1,4 @@
+package com.ouhau.snake;
+
+public class Funciton {
+}
