d2952124afd7562d1c15f041980ec239a2996f21
Admin <75502881@qq.com>
2022-08-24 14:07:17 +0900

    'Go to download site'のリンクを変えた

--- src/com/ouhau/snake/CommonFunction.java
+++ src/com/ouhau/snake/CommonFunction.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 import java.net.URISyntaxException;
 
 public class CommonFunction {
+
     public static void isVisible(JButton button1,JButton button2,JButton button3,boolean isFlg){
         //button set Visible
         button1.setVisible(isFlg);
         button2.setVisible(isFlg);
         button3.setVisible(isFlg);
     }
+
     public static void popSound(){
         Sound sound = new Sound();
         try {
