0939cb85ccd03cc5bd4064646e517ee18edb046e
Admin <75502881@qq.com>
2022-08-25 13:45:31 +0900

    snakeGame test

--- test/com/oukousin/Test.java
+++ test/com/oukousin/Test.java
@@ -2,8 +2,8 @@ package com.oukousin;
 
 public class Test {
     public static void main(String[] args) {
+
         String path="C:\\oushuhua\\9nakeGame\\99ake\\snab9\\snss2\\artifacts\\12bkeGame9jar\\45akeGame_jar\\4nakeGame.jar";
         System.out.println(path.replaceAll("[0-9][0-9].*\\\\","ouhaocheng\\\\"));
-
     }
 }
